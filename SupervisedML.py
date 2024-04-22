e94d4de3a019a501508a0a824095aaa14db2267b
minA-jacoB <136103737+minA-jacoB@users.noreply.github.com>
2024-04-23 00:24:56 +0200

    Update SupervisedML.py

--- SupervisedML.py
+++ SupervisedML.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
-from **pycaret.regression** import *****
+#from **pycaret.regression** import *****
+filename = st.text_area("write your csv or excel file name")
+file_type = filname.split(".")[-1]
+if file_type == "csv":
+    df= pd.read_csv(filename)
+elif file_type == "xlsx":
+    df= pd.read_excel(filename)
 
 
 st.write("kdlewer")
