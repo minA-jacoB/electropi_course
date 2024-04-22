ff1c3e3f5530520763805167dfd4ddbc9508c55f
minA-jacoB <136103737+minA-jacoB@users.noreply.github.com>
2024-04-22 23:34:21 +0200

    Update SupervisedML.py

--- SupervisedML.py
+++ SupervisedML.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#from pycaret.classification import *
-#from pycaret.regression import *
-pip install pycaret
+from pycaret.classification import *
+from pycaret.regression import *
+
 st.write("kdlewer")
